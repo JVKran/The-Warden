39442ff74d96c0883820fd5b97f6c7ccd84bd940
JVKran <jochem.vankranenburg@student.hu.nl>
2019-12-13 12:18:25 +0100

    Implemented face recognition

--- Vision/main.cpp
+++ Vision/main.cpp
@@ -1,56 +1,64 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <iostream>
- 
+#include <stdio.h>
 using namespace std;
 using namespace cv;
- 
-int main(){
- 
-  // Create a VideoCapture object and use camera to capture the video
-  VideoCapture cap(0); 
- 
-  // Check if camera opened successfully
-  if(!cap.isOpened())
-  {
-    cout << "Error opening video stream" << endl; 
-    return -1; 
-  } 
- 
-  // Default resolution of the frame is obtained.The default resolution is system dependent. 
-  int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH); 
-  int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT); 
-   
-  // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file. 
-  VideoWriter video("outcpp.mp4",CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height)); 
-  while(1)
-  { 
-    Mat frame; 
-     
-    // Capture frame-by-frame 
-    cap >> frame;
-  
-    // If the frame is empty, break immediately
-    if (frame.empty())
-      break;
-     
-    // Write the frame into the file 'outcpp.avi'
-    video.write(frame);
-    
-    // Display the resulting frame    
-    imshow( "Frame", frame );
-  
-    // Press  ESC on keyboard to  exit
-    char c = (char)waitKey(1);
-    if( c == 27 ) 
-      break;
-  }
- 
-  // When everything done, release the video capture and write object
-  cap.release();
-  video.release();
- 
-  // Closes all the windows
-  destroyAllWindows();
-  return 0;
+void detectAndDisplay( Mat frame );
+String face_cascade_name = "haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+String window_name = "Capture - Face detection";
+int main( void )
+{
+    VideoCapture capture;
+    Mat frame;
+    //-- 1. Load the cascades
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
+    //-- 2. Read the video stream
+    capture.open( -1 );
+    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+    while ( capture.read(frame) )
+    {
+        if( frame.empty() )
+        {
+            printf(" --(!) No captured frame -- Break!");
+            break;
+        }
+        //-- 3. Apply the classifier to the frame
+        detectAndDisplay( frame );
+        char c = (char)waitKey(10);
+        if( c == 27 ) { break; } // escape
+    }
+    return 0;
+}
+void detectAndDisplay( Mat frame )
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        Mat faceROI = frame_gray( faces[i] );
+        std::vector<Rect> eyes;
+        //-- In each face, detect eyes
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+        for ( size_t j = 0; j < eyes.size(); j++ )
+        {
+            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+    //-- Show what you got
+    imshow( window_name, frame );
 }
