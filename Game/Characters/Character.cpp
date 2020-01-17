35bbf5eb6862ca9281131f7f863b65c5fe46fb9f
1122joshua1 <joshua.vanree@student.hu.nl>
2020-01-17 04:15:25 -0800

    topcollision

--- Game/Characters/Character.cpp
+++ Game/Characters/Character.cpp
@@ -8,22 +8,23 @@ Character::Character(sf::Vector2f position, const std::string & assetName, Asset
 
 void PlayerPhysics::processPhysics(World & world, sf::Vector2f & position, sf::Vector2f &  velocity, const sf::Vector2f & dimensions){
 
-	if(clock.getElapsedTime().asMilliseconds()-lastup>8){
+	if(clock.getElapsedTime().asMilliseconds()-lastup>2){
 	
 	
 	std::vector<SelectableObject> & tiles= world.getTiles();
 	sf::FloatRect tileBounds;
-	bool leftCollision=0, rightCollision=0, bottomCollision=0;
+	bool leftCollision=0, rightCollision=0, bottomCollision=0,topCollision=0;
 
 	sf::FloatRect hitbox = sf::FloatRect(sf::Vector2f(position.x, position.y), sf::Vector2f(dimensions.x, dimensions.y-2));
-	sf::FloatRect bottomHitbox = sf::FloatRect(sf::Vector2f(position.x + 4, position.y ), sf::Vector2f(dimensions.x - 8, dimensions.y + 2));
+	sf::FloatRect bottomHitbox = sf::FloatRect(sf::Vector2f(position.x + 4, position.y+10 ), sf::Vector2f(dimensions.x - 8, dimensions.y -8));
 
 	for(const auto & tile : tiles){
         tileBounds = tile.getBounds();
         if((hitbox.intersects(tile.getBounds()) || bottomHitbox.intersects(tile.getBounds())) && tile.isCollidable()){
-        	bottomCollision += tileBounds.contains(bottomHitbox.left, bottomHitbox.top + bottomHitbox.height) || tileBounds.contains(bottomHitbox.left + bottomHitbox.width, bottomHitbox.top + bottomHitbox.height); 
-        	rightCollision += tileBounds.contains(hitbox.left + hitbox.width, hitbox.top) || tileBounds.contains(hitbox.left + hitbox.width, hitbox.top + hitbox.height);
-        	leftCollision += tileBounds.contains(hitbox.left, hitbox.top) || tileBounds.contains(hitbox.left, hitbox.top + hitbox.height);
+        	bottomCollision += tileBounds.intersects(bottomHitbox); 
+        	rightCollision += tileBounds.intersects(sf::FloatRect(hitbox.left+10,hitbox.top,hitbox.width-10,hitbox.height));
+        	leftCollision += tileBounds.intersects(sf::FloatRect(hitbox.left,hitbox.top,hitbox.width-10,hitbox.height));
+			topCollision += tileBounds.intersects(sf::FloatRect(hitbox.left+5,hitbox.top,hitbox.width-10,hitbox.height-5));
        }
     }
 
@@ -66,10 +67,13 @@ void PlayerPhysics::processPhysics(World & world, sf::Vector2f & position, sf::V
 			current = (clock.getElapsedTime().asMilliseconds());
 			elapsed = current - previous;
 			//std::cout<<elapsed<<'\n';
-			if(elapsed > 500){
+			if(elapsed > 700){
 				state = states::FALLING;
 				velocity.y = 0;
 				break;
+			}
+			if(topCollision){
+				state=states::FALLING;
 			}
 			velocity.y = -5;
 			break;
@@ -89,10 +93,10 @@ void PlayerPhysics::processPhysics(World & world, sf::Vector2f & position, sf::V
 void PlayerInput::processInput(sf::Vector2f & velocity){
 	velocity.x = 0;	//Stand still
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-		velocity.x = -2;
+		velocity.x = -4;
 	}
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-		velocity.x = 2;
+		velocity.x = 4;
 	}
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
 		velocity.y -=1;
