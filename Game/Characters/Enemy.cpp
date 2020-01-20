d7fe8220587015e51a2c65a50cde57153465632e
JVKran <jochem.vankranenburg@student.hu.nl>
2020-01-20 18:37:41 +0100

    >

---
+++ Game/Characters/Enemy.cpp
@@ -0,0 +1,22 @@
+#include "Enemy.hpp"
+
+void EnemyInput::processInput(sf::Vector2f & velocity, const sf::Vector2f & position, const std::vector<Character> & characters){
+	for(const Character & character : characters){
+		if(character.isPlayer() && position.x + 1000 < character.getPosition().x && position.x - 1000 > character.getPosition().x){
+			if(character.getPosition().x > position.x){
+				velocity.x += 5;
+			} else {
+				velocity.x -= 5;
+			}
+		}
+	}
+}
+
+EnemyGraphics::EnemyGraphics(const std::string & assetName, AssetManager & assets):
+	GraphicsComponent(assetName, assets)
+{}
+
+void EnemyGraphics::processGraphics(sf::RenderWindow & window, const sf::Vector2f & position, sf::View & view){
+	sprite.setPosition(position);
+	window.draw(sprite);
+}
---
+++ Game/Characters/Enemy.hpp
@@ -0,0 +1,27 @@
+#ifndef __ENEMY_HPP
+#define __ENEMY_HPP
+
+#include <SFML/Graphics.hpp>
+#include "World.hpp"
+#include "AssetManager.hpp"
+#include "Character.hpp"
+#include "SpriteAnimation.hpp"
+
+// class EnemyPhysics : public PhsysicsComponent {
+
+// };
+
+class EnemyInput : public InputComponent {
+	public:
+		virtual void processInput(sf::Vector2f & velocity, const sf::Vector2f & position, const std::vector<Character> & characters) override;
+};
+
+class EnemyGraphics : public GraphicsComponent{
+	public:
+		EnemyGraphics(const std::string & assetName, AssetManager & assets);
+
+		virtual void processGraphics(sf::RenderWindow & window, const sf::Vector2f & position, sf::View & view) override;
+};
+
+
+#endif //__ENEMY_HPP
