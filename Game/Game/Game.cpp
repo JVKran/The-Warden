1afb67ea069b089a483193b9401e7f504b8ecc46
mennovdjagt <menno.vanderjagt@student.hu.nl>
2020-01-24 15:21:16 +0100

    deleted addTile function when right mouse button pressed

--- Game/Game/Game.cpp
+++ Game/Game/Game.cpp
@@ -46,9 +46,10 @@ void Game::handleEvent(const sf::Event & event, sf::View & view){
 	}
 
 	if( event.type == sf::Event::MouseButtonReleased ){
-		if( event.mouseButton.button == sf::Mouse::Right ){
+		/*if( event.mouseButton.button == sf::Mouse::Right ){
 			characters.at(0).addTile(event, world, window, view);		//adds a new tile using the character player
-		}else if( event.mouseButton.button == sf::Mouse::Middle ){
+		}*/
+		if( event.mouseButton.button == sf::Mouse::Middle ){
 			characters.at(0).deleteTile(event, world, window, view);	//deletes a block using player if the mouse is on the tile
 		}
 	}
