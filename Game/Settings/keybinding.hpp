bf35a07a4afa432676414dcc263b2e2d63d9f19b
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2020-01-30 18:30:34 +0100

    commands added

--- Game/Settings/keybinding.hpp
+++ Game/Settings/keybinding.hpp
@@ -21,9 +21,9 @@ private:
   		"Numpad8", "Numpad9", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "Pause", "KeyCount", "Dash", "BackSpace", "BackSlash", "SemiColon", "Enter" 
 	};
 
-	std::string keyName;
-	sf::Keyboard::Key key;
-	Text text;
+	std::string keyName;		//!< A discribtion of what the key does.
+	sf::Keyboard::Key key;		//!< The key we are using for this object.
+	Text text;			//!< The text object which is used to draw the information above.
 
 public:
 
@@ -39,8 +39,7 @@ public:
 	/// \brief
 	/// This changes the keyName string
 	/// \details
-	/// This function is for when you need to change what the key does describtion for some reason,
-	/// this will also change the string in the text object
+	/// This function is for when you need to change what the key does describtion for some reason.
 	/// @param newKeyName The new string object that describes what the key does.
 	void setString( std::string newKeyName );
 
@@ -66,47 +65,55 @@ public:
 	sf::Keyboard::Key getKey();
 
 	/// \brief
-	/// function to check for collision
+	/// Function to check for collision
 	/// \details
 	/// This function returns if there is a collision between 
 	/// two objects.
-	/// \return a bool if there is a collision
+	/// \return a bool if there is a collision.
 	bool contains( const sf::Vector2f& object ) const;
 
 	/// \brief
-	/// 
+	/// Function to check for collision
 	/// \details
-	/// 
+	/// This function will return if there is collision between two
+	/// objects, this function is meant for mouse objects.
+	/// \return a bool if there is a collision.
 	bool contains( const sf::Vector2i& object );
 
 	/// \brief
-	/// 
+	/// casts a Vector2i to Vector2f
 	/// \details
-	/// 
+	/// This function will cast a Vector2i to a Vector2f.
+	/// @param target the Vector2i object we want to change to a Vector2f.
+	/// \return the Vector2i we cast to Vector2f.
 	sf::Vector2f castToF( sf::Vector2i target );
 
 	/// \brief
-	/// 
+	/// Sets the text of the Text object
 	/// \details
-	/// 
+	/// This function will change the text inside the Text object.
+	/// @param newName A string that will replace the string in the Text object
 	void setText( std::string newName );
 
 	/// \brief
-	/// 
+	/// Gets the text from the Text object
 	/// \details
-	/// 
+	/// This function will get the text inside the Text object.
+	/// \return A string that holds the text from the Text object.
 	std::string getText();
 
 	/// \brief
-	/// 
+	/// sets the color of the Text object
 	/// \details
-	/// 
+	/// This function sets the color of the Text object.
+	/// @param color the color we want the text to have.
 	void setColor(sf::Color color);
 
 	/// \brief
-	/// 
+	/// Draws the object to the screen
 	/// \details
-	/// 
+	/// This function will draw the Text object to the screen.
+	/// \return window This is a sf::RenderWindow object, it is meant to be used to draw to the screen.
 	void draw( sf::RenderWindow & window );
 
 };
